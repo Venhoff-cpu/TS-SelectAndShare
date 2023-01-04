49ccc3bae2a985f62cc57e668c1e4d3e56c6c759
krzysztof <krzysztof.pacuszka@gmail.com>
2023-01-04 12:14:42 +0100

    reformat

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,16 @@ const addressInput = <HTMLInputElement>document.getElementById("address")!;
 const GOOGLE_API_KEY = "GOOGLE_API";
 
 type googleGeocodingResponse = {
-    results: {geometry: {location: {lat: number, lng: number}}}[],
+    results: { geometry: { location: { lat: number, lng: number } } }[],
     status: "OK" | "ZERO_RESULTS";
 }
 const searchAddressHandler = (event: Event) => {
     event.preventDefault()
     const enteredAddress = addressInput.value;
-
-    axios.get<googleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}\n&key=${GOOGLE_API_KEY}`)
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}\n&key=${GOOGLE_API_KEY}`
+    axios.get<googleGeocodingResponse>(url)
         .then(r => {
-            if (r.data.status !== "OK"){
+            if (r.data.status !== "OK") {
                 throw new Error("Could not fetch location!")
             }
             const cordinates = r.data.results[0].geometry.location;
